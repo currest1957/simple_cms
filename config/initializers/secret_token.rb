475392f1f5592cbc513d867d4f86ed5c66309951
Steven Curren <dev@stevencurren.id.au>
2012-12-28 19:24:57 +1100

    Prior to migration exercise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '9ac6da6f58ea800e5fa74690ff250674df43d6ecab0d477c92b13fc4290474f85d81fe516f768c0a2ea5b3cdca39cd0fb2decd49b3602c1f5362c75d2b8d804d'
